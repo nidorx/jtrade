46397dedac79fdabcbcf8c4c161b9d932d388be2
nidorx <contato@alexrodin.info>
2018-09-12 03:09:42 -0300

    Melhorias nos svgs

--- docs/gerar-svg-candles.js
+++ docs/gerar-svg-candles.js
@@ -105,9 +105,11 @@ function candlestickSVG(ticks) {
 
     return [
         '<?xml version="1.0" encoding="utf-8"?>',
+        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
         '<svg ',
         '  version="1.1"',
         '  xmlns="http://www.w3.org/2000/svg"',
+        '  xmlns:xlink="http://www.w3.org/1999/xlink"',
 //        '  style="width: ' + CHART_WIDTH + 'px; height: ' + CHART_HEIGHT + 'px; top: 0px; left: -0.5px;"',
         '  x="0px" y="0px" width="' + CHART_WIDTH + 'px" height="' + CHART_HEIGHT + 'px"',
         '  >',
